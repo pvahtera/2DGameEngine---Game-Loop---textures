a9f17aeecbe6529ce6699c1fd1895730ff177f21
pjvah <pjvah@LAPTOP-FHVFITR4>
2020-08-14 09:39:52 +0300

    render layers and grouping  - half completed

--- BirchEngine/Src/ECS/ECS.h
+++ BirchEngine/Src/ECS/ECS.h
@@ -8,15 +8,17 @@
 
 class Component;
 class Entity;
+class Manager;
 
 // We need an ID for each of our component (position, physics, AI, input handling etc.
 
 using ComponentID = std::size_t;
+using Group = std::size_t;
 
-inline ComponentID getComponentTypeID()
+inline ComponentID getNewComponentTypeID()
 {
 
-	static ComponentID lastID = 0; // every time we call this last id will remember what it was last
+	static ComponentID lastID = 0u; // unsigned, every time we call this last id will remember what it was last
 	return lastID++; // every time we call it we will get the next one
 }
 
@@ -30,7 +32,9 @@ template <typename T> inline ComponentID getComponentTypeID() noexcept
 
 // allows us i.e. to find out if an entity has a selection of components
 constexpr std::size_t maxComponents = 32; 
+constexpr std::size_t maxGroups = 32;	
 using ComponentBitSet = std::bitset<maxComponents>;
+using GroupBitset = std::bitset<maxGroups>;
 using ComponentArray = std::array<Component*, maxComponents>; 
 
 
@@ -53,13 +57,17 @@ public:
 class Entity
 {
 private:
+
+	Manager& manager;
 	bool active = true;
 	std::vector<std::unique_ptr<Component>> components;
 
 	ComponentArray componentArray;
 	ComponentBitSet componentBitSet;
+	GroupBitset groupBitset;
 
 public:
+	Entity(Manager& mManager) : manager(mManager) {}
 	void update()
 	{
 		for (auto& c : components) c->update(); 
@@ -71,6 +79,19 @@ public:
 	}
 	bool isActive() const { return active; }
 	void destroy() { active = false; }
+
+	// we need to be able to add to grp and remove from grp
+
+	void addGroup(Group mGroup);
+	void delGroup(Group mGroup)
+	{
+		groupBitset[mGroup] = false; // if false, remove entity from that grp
+	}
+
+	bool hasGroup(Group mGroup)
+	{
+		return groupBitset[mGroup];
+	}
 
 	template <typename T> bool hasComponent() const
 	{
@@ -105,6 +126,8 @@ class Manager
 private:
 	// vector to hold a list of all our entities
 	std::vector<std::unique_ptr<Entity>> entities;
+	// array of vectors that are groups
+	std::array<std::vector<Entity*>, maxGroups> groupedEntities;
 
 public:
 	void update()
@@ -119,6 +142,19 @@ public:
 	//remove the entities that are not there
 	void refresh()
 	{
+		// remove entities from grp
+		for (auto i(0u); i < maxGroups; i++)
+		{
+			auto& v(groupedEntities[i]); // reference for each grp
+			v.erase(
+				std::remove_if(std::begin(v), std::end(v),
+					[i](Entity* mEntity)
+					{
+						return !mEntity->isActive() || !mEntity->hasGroup(i);
+					}),
+				std::end(v));
+		}
+
 		entities.erase(std::remove_if(std::begin(entities), std::end(entities),
 			[](const std::unique_ptr<Entity> &mEntity)
 			{
@@ -127,11 +163,23 @@ public:
 		std::end(entities));
 	}
 
+	// have manager add to the grp
+
+	void AddToGroup(Entity* mEntity, Group mGroup)
+	{
+		groupedEntities[mGroup].emplace_back(mEntity);
+	}
+
+	std::vector<Entity*>& getGroup(Group mGroup)
+	{
+		return groupedEntities[mGroup];
+	}
+
 	// add an entity to our world through our manager class
 
 	Entity& addEntity()
 	{
-		Entity* e = new Entity();
+		Entity* e = new Entity(*this);
 		std::unique_ptr<Entity> uPtr{ e };
 		// add to our entity list
 		entities.emplace_back(std::move(uPtr));
