eeda089617612f528256c5b9fac06d235e34224b
pjvah <pjvah@LAPTOP-FHVFITR4>
2020-08-14 10:25:37 +0300

    Multiple animations half completed

--- BirchEngine/Src/ECS/SpriteComponent.h
+++ BirchEngine/Src/ECS/SpriteComponent.h
@@ -3,6 +3,9 @@
 #include "Components.h"
 #include "SDL.h"
 #include "../TextureManager.h"
+#include "Animation.h"
+#include <map>
+
 
 class SpriteComponent : public Component
 {
@@ -18,17 +21,29 @@ private:
 	int speed = 100; // delay between frames in milliseconds
 
 public:
+
+	int animIndex = 0;
+
+	// this will hold our animations
+	std::map<const char*, Animation> animations;
+
 	SpriteComponent() = default;
 	SpriteComponent(const char* path)
 	{
 		setTex(path); // load texture here
 	}
 
-	SpriteComponent(const char* path, int nFrames, int mSpeed)
+	SpriteComponent(const char* path, bool isAnimated)
 	{
-		animated = true;
-		frames = nFrames;
-		speed = mSpeed;
+		animated = isAnimated;
+
+		Animation idle = Animation(0, 3, 100);
+		Animation walk = Animation(1, 8, 100);
+
+		animations.emplace("Idle", idle);
+		animations.emplace("Walk", walk);
+
+		Play("Idle");
 		setTex(path); // load texture here
 	}
 
@@ -61,6 +76,8 @@ public:
 			srcRect.x = srcRect.w * static_cast<int>((SDL_GetTicks() / speed) % frames);
 		}
 
+		srcRect.y = animIndex * transform->height;
+
 		// this function is for moving around
 		// rectangle expects int so that's why we cast them
 		destRect.x = (int)transform->position.x;
@@ -73,4 +90,12 @@ public:
 	{
 		TextureManager::Draw(texture, srcRect, destRect);
 	}
+
+	void Play(const char* animName)
+	{
+		frames = animations[animName].frames;
+		animIndex = animations[animName].index;
+		speed = animations[animName].speed;
+	}
+
 };
